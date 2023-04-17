e0c12ff130a1bac7d11031a3569a5a724eb3e7ab
julien-vaz <jhmvaz95@gmail.com>
2023-04-16 22:29:42 -0300

    Implementations of LCG and LFSR done

--- lfsr.py
+++ lfsr.py
@@ -1,28 +1,31 @@
-import time # Para calcular o tempo gasto e gerar a semente
+import time
 
 
-class LCG:
-    # Parametros
-    def __init__(self, mult, incr, bits):
-        self.a = mult
-        self.c = incr
-        self.m = 2 ** bits - 1 # Escolhi usar um Mersenne Prime como modulo
-        self.state = int(time.time())
+class LFSR:
+    def __init__(self, bits, taps):
+        self.state = 1 << bits | 1
+        self.taps = taps
         # Parametro adicional para controlar o numero de bits
         self.bits = bits
-
-    # Funcao geradora
+    
     def generate(self):
-        self.state = (self.a * self.state + self.c) % self.m
-        return self.state << (self.bits - self.state.bit_length())
+        # Calcula o XOR dos bits tocados
+        xor = 0
+        for tap in self.taps:
+            xor ^= (self.state >> tap) & 1
+        
+        # Desloca state e adiciona o resultado de XOR ao bit menos significativo
+        self.state = (self.state >> 1) | (xor << (self.bits - 1))
+        
+        return self.state
 
 # Funcao global para salvar os numeros gerados em um TXT
-def save_as_txt(bits, lcg):
-    numbers = open(f"lcg_numbers_{bits}.txt", "w")
+def save_as_txt(bits, lfsr):
+    numbers = open(f"lfsr_numbers_{bits}.txt", "w")
     numbers.write("Number, Bits, Time required (s)\n")
     for _ in range(100):
         start_time = time.monotonic()
-        number = lcg.generate()
+        number = lfsr.generate()
         end_time = time.monotonic()
 
         # Calculo do tempo gasto
@@ -33,143 +36,134 @@ def save_as_txt(bits, lcg):
 
 # -----------------------------------------------------------
 
+# REFERENCE: Xilinx Application Note by Peter Alfke XAPP 052 July 7,1996 (Version 1.1)
+
 # Parametros para numeros de 40 bits
-mult = 1664525
-incr = 1013904223
 bits = 40
+taps = [40,38,21,19]
 
 # Gerador de 40 bits
-lcg_40 = LCG(mult, incr, bits)
+lfsr_40 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um TXT, para analise posterior
-save_as_txt(bits, lcg_40)
+save_as_txt(bits, lfsr_40)
 
 #-------------------------------------------------------------
 
 # Parametros para numeros de 56 bits
-mult = 1103515245
-incr = 12345
 bits = 56
+taps = [56,55,35,34]
 
 # Gerador de 56 bits
-lcg_56 = LCG(mult, incr, bits)
+lfsr_56 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_56)
+save_as_txt(bits, lfsr_56)
 
 # --------------------------------------------------------------
 
 # Parametros para numeros de 80 bits
-mult = 6364136223846793005
-incr = 1442695040888963407
 bits = 80
+taps = [80,79,43,42]
 
 # Gerador de 80 bits
-lcg_80 = LCG(mult, incr, bits)
+lfsr_80 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_80)
+save_as_txt(bits, lfsr_80)
 
 # ---------------------------------------------------------------
 
 # Parametros para numeros de 128 bits
-mult = 4294957665
-incr = 1372765723
 bits = 128
+taps = [128,126,101,99]
 
 # Gerador de 128 bits
-lcg_128 = LCG(mult, incr, bits)
+lfsr_128 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_128)
+save_as_txt(bits, lfsr_128)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 168 bits
-mult = 103067
-incr = 123456789
 bits = 168
+taps = [168,166,153,151]
 
 # Gerador de 168 bits
-lcg_168 = LCG(mult, incr, bits)
+lfsr_168 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_168)
+save_as_txt(bits, lfsr_168)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 224 bits
-mult = 1125899906842597
-incr = 987654321
 bits = 224
+taps = [223,220,210,209]
 
 # Gerador de 224 bits
-lcg_224 = LCG(mult, incr, bits)
+lfsr_224 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_224)
+save_as_txt(bits, lfsr_224)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 256 bits
-mult = 1103515245
-incr = 12345
 bits = 256
+taps = [255,251,247,246]
 
 # Gerador de 256 bits
-lcg_256 = LCG(mult, incr, bits)
+lfsr_256 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_256)
+save_as_txt(bits, lfsr_256)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 512 bits
-mult = 69069687767179458030604164730607065059
-incr = 2531011
 bits = 512
+taps = [511,507,503,499]
 
 # Gerador de 512 bits
-lcg_512 = LCG(mult, incr, bits)
+lfsr_512 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_512)
+save_as_txt(bits, lfsr_512)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 1024 bits
-mult = 6193318378121905469
-incr = 284470788008853
 bits = 1024
+taps = [1023,1019,1013,1009]
 
 # Gerador de 1024 bits
-lcg_1024 = LCG(mult, incr, bits)
+lfsr_1024 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_1024)
+save_as_txt(bits, lfsr_1024)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 2048 bits
-mult = 9949325612958798629
-incr = 9949325612958798629
 bits = 2048
+taps = [2047,2039,2035,2029]
 
 # Gerador de 2048 bits
-lcg_2048 = LCG(mult, incr, bits)
+lfsr_2048 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_2048)
+save_as_txt(bits, lfsr_2048)
 
 # -----------------------------------------------------------------
 
 # Parametros para numeros de 4096 bits
-mult = 10509375158889068245
-incr = 10509375158889068245
 bits = 4096
+taps = [4095,4079,4073,4057]
 
 # Gerador de 4096 bits
-lcg_4096 = LCG(mult, incr, bits)
+lfsr_4096 = LFSR(bits, taps)
 
 # Salvo os numeros gerados em um novo TXT, para analise posterior
-save_as_txt(bits, lcg_4096)
+save_as_txt(bits, lfsr_4096)
